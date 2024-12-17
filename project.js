b5094c43bf7ca50023c4b513fa4562c9caa05bfe
Daksh-Singh5 <dakshsingh6612@gmail.com>
2024-12-17 19:23:44 +0530

    heloo

---
+++ project.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    answer=a+b
+    document.write(answer,"<br>")
+}
+function subtract(a,b){
+    answer=a-b
+    document.write(answer,"<br>")
+}
+function times(a,b){
+    answer=a*b
+    document.write(answer,"<br>")
+}
+function divied(a,b){
+    answer=a/b
+    document.write(answer,"<br>")
+}
+add(2,2)
+subtract(2,2)
+times(2,2)
+divied(2,2)
